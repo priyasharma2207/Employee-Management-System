3a71ad880dc97725895da132ec30f094c655e9c6
Priya Sharma <235349386+priyasharma2207@users.noreply.github.com>
2026-02-25 23:01:59 +0530

    Add files via upload

---
+++ Employee Management System/src/Employee/Management/System/conn.java
@@ -0,0 +1,21 @@
+package Employee.Management.System;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class conn {
+
+    Connection connection;
+    Statement statement;
+
+    public conn(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ employeemanagement","root","raj_sharma2205");
+            statement = connection.createStatement();
+        }catch (Exception e){
+            e. printStackTrace();
+        }
+    }
+}
